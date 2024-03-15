ea41e60379f9ff87e75e60b73be5ebd8eb331ba7
Mehul Batra <mehulbtr@gmail.com>
2024-03-15 19:56:46 +0530

    Your commit message here

---
+++ test.js
@@ -0,0 +1,14 @@
+// function updateCounter(){
+//     let counter = 0
+//     if(counter<500){
+//         counter += 1
+//         console.log(counter)
+//     }
+// }
+// updateCounter();
+// let counter = 0
+
+for(i=0;i<=200;i++){
+    
+    console.log(i)
+}
